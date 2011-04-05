8e4bab27622b8a7e26e42b3e2ff244d8917d7950
NaveenPramod <naveenkumta@yahoo.co.in>
2011-04-05 21:04:50 +0100

    FIRST COMMIT

---
+++ src/main/java/com/naveen/test/dvo/Department.java
@@ -0,0 +1,45 @@
+package com.naveen.test.dvo;
+
+public class Department {
+	private Long departmentId;
+	private String departmentName;
+	public Long getDepartmentId() {
+		return departmentId;
+	}
+	public void setDepartmentId(Long departmentId) {
+		this.departmentId = departmentId;
+	}
+	public String getDepartmentName() {
+		return departmentName;
+	}
+	public void setDepartmentName(String departmentName) {
+		this.departmentName = departmentName;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((departmentId == null) ? 0 : departmentId.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Department other = (Department) obj;
+		if (departmentId == null) {
+			if (other.departmentId != null)
+				return false;
+		} else if (!departmentId.equals(other.departmentId))
+			return false;
+		return true;
+	}
+	
+	
+	
+}
---
+++ src/main/java/com/naveen/test/dvo/Employee.java
@@ -0,0 +1,73 @@
+package com.naveen.test.dvo;
+
+import java.util.List;
+
+public class Employee {
+	
+	private Long employeeId;
+	private String firstName;
+	private String lastName;	
+	private Department department;// = new Department();
+	private List<Employee> subordinates;
+	
+	public Long getEmployeeId() {
+		return employeeId;
+	}
+	public void setEmployeeId(Long employeeId) {
+		this.employeeId = employeeId;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	public Department getDepartment() {
+		return department;
+	}
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+
+    public List<Employee> getSubordinates() {
+        return subordinates;
+    }
+
+    public void setSubordinates(List<Employee> subordinates) {
+        this.subordinates = subordinates;
+    }
+
+    @Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((employeeId == null) ? 0 : employeeId.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Employee other = (Employee) obj;
+		if (employeeId == null) {
+			if (other.employeeId != null)
+				return false;
+		} else if (!employeeId.equals(other.employeeId))
+			return false;
+		return true;
+	}
+	
+	
+}
